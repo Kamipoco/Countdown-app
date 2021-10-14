872ae1d0ccd1469f20c24f46b3bf7e0dd21c65f5
Van Lam <vanlam020499@gmail.com>
2021-10-14 17:04:03 +0700

    second commit

---
+++ app.js
@@ -0,0 +1,41 @@
+var NGAYTET = new Date("January 1, 2022, 00:00:00").getTime();
+
+setInterval(function () {
+  var HIENTAI = new Date().getTime();
+  var D = NGAYTET - HIENTAI;
+
+  //hiển thị ngày, giờ, phút giây
+  var days = Math.floor(D / (1000 * 60 * 60 * 24));
+  var hours = Math.floor(D / (1000 * 60 * 60));
+  var minutes = Math.floor(D / (1000 * 60));
+  var seconds = Math.floor(D / 1000);
+
+  hours %= 24;
+  minutes %= 60;
+  seconds %= 60;
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+}, 1000);
+
+// setInterval(() => {
+//     var HIENTAI = new Date.getTime();
+//     var D = (NGAYTET = HIENTAI);
+
+//     //hiển thị ngày, giờ, phút giây
+//     var days = Math.floor(D / (1000 * 60 * 60 * 24));
+//     var hours = Math.floor(D / (1000 * 60 * 60));
+//     var minutes = Math.floor(D / (1000 * 60));
+//     var seconds = Math.floor(D / 1000);
+
+//     hours %= 24;
+//     minutes %= 60;
+//     seconds %= 60;
+
+//     document.getElementById("days").innerText = days;
+//     document.getElementById("hours").innerText = hours;
+//     document.getElementById("minutes").innerText = minutes;
+//     document.getElementById("seconds").innerText = seconds;
+//   }, 1000);
